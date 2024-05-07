67ce7ac5272eded23ac71d0e550cc8c67a5f8a4a
Antti Juustila <antti.juustila@gmail.com>
2024-05-07 17:20:32 +0300

    Changed the way app is launched. Code formatting changes.

--- src/GUIApp.java
+++ src/GUIApp.java
@@ -2,20 +2,23 @@ import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 public class GUIApp {
     public static void main(String[] args) throws Exception {
-        new GUIApp().launch();
+        GUIApp.model = new DotModel(WIDTH, HEIGHT);
+        SwingUtilities.invokeLater(() -> {
+            new GUIApp().launch();
+        });
     }
 
     private JFrame mainFrame;
-    private DotModel model;
+    private static DotModel model;
     private static final int WIDTH = 400;
     private static final int HEIGHT = 200;
-    
+
     private void launch() {
-        model = new DotModel(WIDTH, HEIGHT);
         GraphicsPanel panel = new GraphicsPanel(model);
         panel.setSize(WIDTH, HEIGHT);
         model.setObserver(panel);
--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -10,7 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class GraphicsPanel extends JPanel implements DotModelObserver, ComponentListener, MouseListener  {
+public class GraphicsPanel extends JPanel implements DotModelObserver, ComponentListener, MouseListener {
 
     private JLabel dotsCountLabel = new JLabel("1 dot");
     private DotModel model;
@@ -30,7 +30,7 @@ public class GraphicsPanel extends JPanel implements DotModelObserver, Component
         final List<Dot> dots = model.getDots();
         for (final Dot dot : dots) {
             g.setColor(dot.color);
-            g.fillOval((int) dot.point.getX(), (int)dot.point.getY(), DotModel.DOT_SIZE, DotModel.DOT_SIZE);
+            g.fillOval((int) dot.point.getX(), (int) dot.point.getY(), DotModel.DOT_SIZE, DotModel.DOT_SIZE);
         }
     }
 
@@ -41,19 +41,20 @@ public class GraphicsPanel extends JPanel implements DotModelObserver, Component
         // handling thread. Try to comment out lines #1 and #2 and see from the log
         // that then the code is executed from the DotModel thread. In some cases
         // this will lead to exceptions thrown, indicating that wrong thread is using
-        // The Swing components. In some cases it does not. Unsure why that happens or not.
+        // The Swing components. In some cases it does not. Unsure why that happens or
+        // not.
         SwingUtilities.invokeLater(() -> { // #1
             // System.out.format("Thread: %s%n", Thread.currentThread().getName());
             int dotCount = model.getDotCount();
             dotsCountLabel.setText(String.format("%d dot%s", dotCount, dotCount != 1 ? "s" : ""));
-           repaint();
+            repaint();
         }); // #2
     }
 
     @Override
     public void componentResized(ComponentEvent e) {
         Dimension newDimension = getSize();
-        model.setSize((int)newDimension.getWidth(), (int)newDimension.getHeight());
+        model.setSize((int) newDimension.getWidth(), (int) newDimension.getHeight());
     }
 
     @Override
@@ -92,5 +93,5 @@ public class GraphicsPanel extends JPanel implements DotModelObserver, Component
     @Override
     public void mouseExited(MouseEvent e) {
     }
-    
+
 }
